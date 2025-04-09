9ce792c14738cfb0dc45b45dfe0a9820a60b22c9
Angel <angelgaelguevara@gmail.com>
2025-04-09 16:12:23 -0600

    Error DB

---
+++ resources/views/errors/database_unavailable.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🚧 Mantenimiento 🚧</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body class="text-center">
+    <div class="container mt-5">
+        <h1 class="display-4 text-danger">🚧 Mantenimiento 🚧</h1>
+        <p class="lead">Nuestro sistema está en mantenimiento debido a problemas con la base de datos. Por favor, intenta más tarde.</p>
+    </div>
+</body>
+</html>
+
+
+
